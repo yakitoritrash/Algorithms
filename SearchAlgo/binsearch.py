7cb8fd9a67fa2448c9b37772f343ded2d3c7e5f1
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-02-04 16:15:26 +0530

    binarysearch

--- SearchAlgo/binsearch.py
+++ SearchAlgo/binsearch.py
@@ -1,18 +1,20 @@
 def binary_search(list, item):
-    low - 0
+    low = 0
     high = len(list) - 1
 
     while low <= high:
-        mid = (low+high)
+        mid = (low+high)//2
         guess = list[mid]
 
         if guess == item:
             return mid
-        if guess > item:
+        elif guess > item:
             high = mid - 1
-        else:
+        elif guess < item:
             low = mid + 1
-        return None
+        else:
+            return None
+        
 
 my_list = [1, 3, 5, 7, 9]
 
