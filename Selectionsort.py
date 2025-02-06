fe27f8cc866e9f513af548ae88c3e3c677cfb758
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2025-02-06 22:43:36 +0530

    selection

--- Selectionsort.py
+++ Selectionsort.py
@@ -1,7 +1,7 @@
 def findSmallest(arr):
     smallest = arr[0]
     smallest_index = 0
-    for in range(1, len(arr)):
+    for i in range(1, len(arr)):
         if arr[i] < smallest:
             smallest = arr[i]
             smallest_index = i
