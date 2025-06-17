94c417d1ef45684313dfc588d716070cdb9e251b
Arch me <kishudwivedi30@gmail.com>
2025-06-17 15:53:18 +0530

    ana

---
+++ anagram.js
@@ -0,0 +1,13 @@
+isAnagram = function(s, t) {
+  if (s.length !== t.length) return false;
+  
+  const count = {};
+  for (let char of s) {
+    count[char] = (count[char] || 0) + 1;
+  }
+  for (let char of t) {
+    if (!count[char]) return false;
+    count[char]--;
+  }
+  return true;
+}
