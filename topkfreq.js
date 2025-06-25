7c3b42054f07bfa463f0d951234e33b1aa605291
Arch me <kishudwivedi30@gmail.com>
2025-06-25 15:55:17 +0530

    top k freq

--- topkfreq.js
+++ topkfreq.js
@@ -1,1 +1,21 @@
-var 
+var topKFreq = function (nums, k) {
+  const freqMap = new Map();
+  const buckets = [];
+  const result = [];
+
+  for (let num of nums) {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  }
+
+  for (let [num, freq] of freqMap) {
+    buckets[freq] = (buckets[freq] || new Set()).add(nums);
+  }
+
+  for (let i = buckets.length-1; i >= 0; i--) {
+    if (buckets[i]) {
+      result.push(...buckets[i]);
+    }
+    if (result.length === k) break;
+  }
+  return result;
+}
