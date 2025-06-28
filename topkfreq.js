7388d8b605fc2c853974ffdc25f90e3aae0d1d1c
Arch me <kishudwivedi30@gmail.com>
2025-06-28 17:46:11 +0530

    top k frequent elements

--- topkfreq.js
+++ topkfreq.js
@@ -1,21 +1,23 @@
-var topKFreq = function (nums, k) {
-  const freqMap = new Map();
+var topFreqK = function(nums, k) {
+  const freqmap = new Map();
   const buckets = [];
   const result = [];
 
   for (let num of nums) {
-    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+    freqmap.set(num, (freqmap.get(num) || 0) + 1);
   }
 
-  for (let [num, freq] of freqMap) {
+  for (let [num, freq] of freqmap) {
     buckets[freq] = (buckets[freq] || new Set()).add(num);
   }
 
-  for (let i = buckets.length-1; i >= 0; i--) {
+  for (let i = buckets.length - 1; i >= 0; i--) {
     if (buckets[i]) {
       result.push(...buckets[i]);
     }
-    if (result.length === k) break;
+    if (result.length === k) {
+      break;
+    }
+    return result;
   }
-  return result;
 }
