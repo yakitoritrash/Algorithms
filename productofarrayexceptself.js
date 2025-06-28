c5fc5e56660d23ff789a01d7f1cf3f23e5ae9637
Arch me <kishudwivedi30@gmail.com>
2025-06-29 01:13:16 +0530

    238

--- productofarrayexceptself.js
+++ productofarrayexceptself.js
@@ -1,12 +1,12 @@
 var productExceptSelf = function (nums) {
-  const output = Array(nums.length).fill(1);
+  const output = Array(nums.length).fill(1)
   let left = 1;
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < output.length; i++) {
     output[i] *= left;
     left *= nums[i];
   }
   let right = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
+  for (let i = output.length - 1; i >= 0; i--) {
     output[i] *= right;
     right *= nums[i];
   }
