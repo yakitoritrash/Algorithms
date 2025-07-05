d0fd137404543c9d3c9c6f035c72dbb8fef081f8
Arch me <kishudwivedi30@gmail.com>
2025-07-06 03:39:36 +0530

    water:

---
+++ trappingrainwater.js
@@ -0,0 +1,25 @@
+function trap (height) {
+  if (!height || height.length === 0) {
+    return 0;
+  }
+  let l = 0;
+  let r = height.length - 1;
+  let leftMax = height[l];
+  let rightMax = height[r];
+  let res = 0;
+  while (l < r) {
+    if (leftMax < rightMax) {
+      l++;
+      leftMax = Math.max(leftMax, height[l]);
+      res += leftMax - height[l];
+    } else {
+      r--;
+      rightMax = Math.max(rightMax, height[r]);
+      res += rightMax - height[l];
+    }
+  }
+  console.log(res);
+  return res;
+}
+
+trap ([0,1,0,2,1,0,1,3,2,1,2,1]);
