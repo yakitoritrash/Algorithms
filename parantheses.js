df59d0d088894244951f556da44e16201d307601
Arch me <kishudwivedi30@gmail.com>
2025-07-07 14:39:16 +0530

    parantheses

--- parantheses.js
+++ parantheses.js
@@ -10,13 +10,17 @@ function isValid(s) {
       if (stack.length > 0 && stack[stack.length - 1] === closeToopen[c]) {
         stack.pop();
       } else {
+        console.log(false);
         return false;
       }
     } else {
       stack.push(c);
     }
   }
+  if (stack.length === 0) {
+    console.log(true);
+  }
   return stack.length === 0;
 }
 
-isValid('((([]{})))');
+isValid('((([]{}))))');
