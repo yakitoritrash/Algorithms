080963faa8081b59785394aa9f9e86573768580d
Arch me <kishudwivedi30@gmail.com>
2025-07-07 13:53:13 +0530

    valid parantheses

--- validparantheses.js
+++ validparantheses.js
@@ -1,7 +1,23 @@
 function validParantheses(s) {
-  arr = [];
-  arr.push(s.split(''));
-  console.log(arr);
+  const arr = [];
+  for (let i = 0; i < s.length; i++) {
+    let c = s.charAt(i);
+    switch(c) {
+      case '(': arr.push(')');
+      break;
+      case '[': arr.push(']');
+      break;
+      case '{': arr.push('}');
+      break;
+      default:
+      if (c !== arr.pop()) {
+        console.log(false);
+        return false;    
+      }
+    }
+  }
+  console.log(true);
+  return arr.length === 0;
 }
 
 validParantheses('()[]{}')
