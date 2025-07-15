4ce1b3d49e42d335ce683889c8f1db0e583718a6
Arch me <kishudwivedi30@gmail.com>
2025-07-15 23:39:37 +0530

    spiral matrix

--- spiralmatrix.js
+++ spiralmatrix.js
@@ -1,13 +1,38 @@
 function spiralMatrix(arr) {
   let res = [];
-  for (let i = 0; i < arr[0].length; i++) {
-    res.push(arr[0][i]);
-  }
-  for (let i = 0; i < arr.length; i++) {
-    res.push(arr[i][arr[0].length - 1]);
-  }
-  for (let i = arr[0].length - 2; i >= 0; i--) {
-    res.push(arr[arr.length - 1][i]);
+  let top = 0, bottom = arr.length - 1;
+  let left = 0, right = arr[0].length - 1;
+//  for (let i = 0; i < arr[0].length; i++) {
+//    res.push(arr[0][i]);
+//  }
+//  for (let i = 0; i < arr.length; i++) {
+//    res.push(arr[i][arr[0].length - 1]);
+//  }
+//  for (let i = arr[0].length - 2; i >= 0; i--) {
+//    res.push(arr[arr.length - 1][i]);
+//  }
+  
+  while (top <= bottom && left <= right) {
+    for (let i = top; i <= right; i++) {
+      res.push(arr[top][i]);
+    }
+    top++;
+    for (let i = top; i <= bottom; i++) {
+      res.push(arr[i][right]);
+    }
+    right--;
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) {
+        res.push(arr[bottom][i]);
+      }
+      bottom--;
+    }
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) {
+        res.push(arr[i][left]);
+      }
+      left++;
+    }
   }
   console.log(res);
 }
