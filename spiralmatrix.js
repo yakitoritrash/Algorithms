4e8ad291538f3bcaafd634a2611d8dd359aebdf3
Arch me <kishudwivedi30@gmail.com>
2025-07-15 19:02:24 +0530

    spiral matrix:

--- spiralmatrix.js
+++ spiralmatrix.js
@@ -3,6 +3,12 @@ function spiralMatrix(arr) {
   for (let i = 0; i < arr[0].length; i++) {
     res.push(arr[0][i]);
   }
+  for (let i = 0; i < arr.length; i++) {
+    res.push(arr[i][arr[0].length - 1]);
+  }
+  for (let i = arr[0].length - 2; i >= 0; i--) {
+    res.push(arr[arr.length - 1][i]);
+  }
   console.log(res);
 }
 
